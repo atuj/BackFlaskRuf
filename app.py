e6dfa754be94f106556e41546bad3151a4922942
Atuj <faffalaffa04@gmail.com>
2022-12-19 21:26:29 +0300

    update

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 import psycopg2
 import json
 from flask import Flask, render_template, request
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 def get_db_connection():
     conn = psycopg2.connect(host='185.241.192.67',
@@ -12,6 +15,7 @@ def get_db_connection():
     return conn
 
 @app.route('/pgetcounthum')
+@cross_origin()
 def index():
     param_hum = request.args.get('value')
     conn = get_db_connection()
@@ -23,6 +27,7 @@ def index():
     return {"hum":valueresult[0][0]}
 
 @app.route('/pgetpass')
+@cross_origin()
 def pavelgetpass():
     param_login = request.args.get('login')
     conn = get_db_connection()
@@ -34,6 +39,7 @@ def pavelgetpass():
     return {"password":passresult[0][0]}
 
 @app.route('/pgetusers')
+@cross_origin()
 def pavelgetusers():
     conn = get_db_connection()
     cur = conn.cursor()
@@ -45,6 +51,7 @@ def pavelgetusers():
     return data
 
 @app.route('/pgettemp')
+@cross_origin()
 def pavelgettemp():
     conn = get_db_connection()
     cur = conn.cursor()
@@ -53,4 +60,44 @@ def pavelgettemp():
     cur.close()
     conn.close()
     data = [{'value':item[0].replace("(","").replace(")","").split(",")[0],'date':item[0].replace("(","").replace(")","").split(",")[1]} for i, item in enumerate(passresult)]
+    #return data
+    warning = ""
+    lastPair = data[len(data)-1]
+    print(lastPair)
+    #jsonView = json.load(lastPair)
+    lastTemperature = lastPair['value']
+    print(lastTemperature)
+
+    if((int(lastTemperature)>25) or (int(lastTemperature) < 17)):
+        warning = "Осторожно, температура за пределами нормы"
+
+    lastDate = lastPair['date']
+    return render_template('index.html',temp=lastTemperature, date =lastDate, warn = warning)
+
+
+@app.route('/ptemplate')
+@cross_origin()
+def paveltemplate():
+    conn = get_db_connection()
+    cur = conn.cursor()
+    cur.execute('select gettemperature()')
+    passresult = cur.fetchall()
+    cur.close()
+    conn.close()
+    data = [{'value': item[0].replace("(", "").replace(")", "").split(",")[0],
+             'date': item[0].replace("(", "").replace(")", "").split(",")[1]} for i, item in enumerate(passresult)]
+    lastPair = data[len(data) - 1]
+    return lastPair
+
+@app.route('/palltempdata')
+@cross_origin()
+def pavelalltempdata():
+    conn = get_db_connection()
+    cur = conn.cursor()
+    cur.execute('select gettemperature()')
+    passresult = cur.fetchall()
+    cur.close()
+    conn.close()
+    data = [{'value': item[0].replace("(", "").replace(")", "").split(",")[0],
+             'date': item[0].replace("(", "").replace(")", "").split(",")[1]} for i, item in enumerate(passresult)]
     return data
